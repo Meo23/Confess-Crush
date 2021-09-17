72cf8ebe593ffde9c25378f3ce193891f5f53149
Meo23 <90922809+Meo23@users.noreply.github.com>
2021-09-17 22:56:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Tình yêu của anh",
+    introTitle: 'ai kia =))',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
